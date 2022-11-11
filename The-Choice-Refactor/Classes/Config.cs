24e4ff439c6d64f72457bd636e506a7e999d4cd7
Volodymyr <67063105+YurkevichVA@users.noreply.github.com>
2022-11-11 21:26:26 +0200

    init commit

---
+++ The-Choice-Refactor/Classes/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace The_Choice_Refactor.Classes
+{
+    public class Config
+    {
+        public string? Theme { get; set; }
+        public string? Currency { get; set; }
+        public string? Language { get; set; }
+    }
+}
---
+++ The-Choice-Refactor/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using The_Choice_Refactor.Classes;
+
+namespace The_Choice_Refactor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Config? config;     // application configuration (theme, language, currency)
+        private Page currentPage;   // page that showed in frame at the moment
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();                                                                               // init http client to work with apis
+            config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(@"..\..\..\UserData\Configuration.json"));  // load configuration from json-file
+            WindowState = WindowState.Maximized;
+        }
+    }
+}
